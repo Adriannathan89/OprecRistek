8fe322e101f89a3ffca0e3c47db3b167c23fe855
Adrian <AdrianPersonal677@gmail.com>
2026-02-21 05:40:42 +0700

    feat: creating section endpoint and integrating auth for section endpoint

--- backend/src/section/section.controller.ts
+++ backend/src/section/section.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Param, Post, Put, Body, Delete } from "@nestjs/common";
+import { Controller, Get, Param, Post, Put, Body, Delete, UseGuards } from "@nestjs/common";
 import { SectionService } from "./section.service";
+import { JwtAuthGuard } from "src/auth-guard/jwt-auth.guard";
+import { Request } from "@nestjs/common";
 
 @Controller("/api/section")
 export class SectionController {
@@ -7,23 +9,27 @@ export class SectionController {
         private readonly sectionService: SectionService
     ) {}
 
+    @UseGuards(JwtAuthGuard)
     @Post("/")
-    async createSection(@Body() sectionDto) {
-        return await this.sectionService.createSection(sectionDto);
+    async createSection(@Body() sectionDto, @Request() req) {
+        return await this.sectionService.createSection(sectionDto, req.user);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get("/:formId")
-    async getSectionByFormId(@Param("formId") formId: string) {
-        return await this.sectionService.getSectionByFormId(formId);
+    async getSectionByFormId(@Param("formId") formId: string, @Request() req) {
+        return await this.sectionService.getSectionByFormId(formId, req.user);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Put("/:id")
-    async updateSection(@Param("id") id: string, @Body() sectionDto) {
-        return await this.sectionService.updateSection(id, sectionDto);
+    async updateSection(@Param("id") id: string, @Body() sectionDto, @Request() req) {
+        return await this.sectionService.updateSection(id, sectionDto, req.user);
     }
 
+    @UseGuards(JwtAuthGuard)
     @Delete("/:id")
-    async deleteSection(@Param("id") id: string) {
-        return await this.sectionService.deleteSection(id);
+    async deleteSection(@Param("id") id: string, @Request() req) {
+        return await this.sectionService.deleteSection(id, req.user);
     }
 }
--- backend/src/section/section.service.ts
+++ backend/src/section/section.service.ts
@@ -4,6 +4,7 @@ import { Section } from "./section.entity";
 import { SectionDto } from "./section.dto";
 import { ApiResponse } from "src/apiResponse/api.response";
 import { Repository } from "typeorm";
+import { Form } from "src/form/form.entity";
 
 @Injectable()
 export class SectionService {
@@ -11,25 +12,51 @@ export class SectionService {
         @InjectRepository(Section) private sectionRepository: Repository<Section>,
     ) { }
 
-    async createSection(sectionDto: SectionDto) {
+    validateSectionOwnership(sectionId: string, userId: string): Promise<boolean> {
+        return this.sectionRepository.manager
+            .createQueryBuilder(Section, "section")
+            .andWhere("section.id = :sectionId", { sectionId })
+            .andWhere("section.userId = :userId", { userId })
+            .getCount()
+            .then(count => count > 0);
+    }
+
+    validateFormPublicationStatus(formId: string): Promise<boolean> {
+        return this.sectionRepository.manager
+            .createQueryBuilder(Form, "form")
+            .andWhere("form.id = :formId", { formId })
+            .andWhere("form.isPublished = true")
+            .getCount()
+            .then(count => count > 0);
+    }
+
+    async createSection(sectionDto: SectionDto, user: any) {
         try {
             const sectionData = new Section();
             Object.assign(sectionData, sectionDto);
+            sectionData.userId = user.sub;
             const section = await this.sectionRepository.save(sectionData);
-            return new ApiResponse<Section>(true, 201, "Section created successfully");
+            return new ApiResponse<Section>(true, 201, "Section created successfully", section);
         } catch (error) {
             const apiResponse = new ApiResponse<Section>(false, 500, "Failed to create section");
             return apiResponse;
         }
     }
 
-    async updateSection(id: string, sectionDto: SectionDto) {
+    async updateSection(id: string, sectionDto: SectionDto, user: any
+    ) {
         try {
             const section = await this.sectionRepository.findOne({ where: { id } });
-            if (!section) {
+            if(!section) {
                 const apiResponse = new ApiResponse<Section>(false, 404, "Section not found");
                 return apiResponse;
             }
+
+            if(!await this.validateSectionOwnership(id, user.sub)) {
+                const apiResponse = new ApiResponse<Section>(false, 403, "You do not have permission to update this section");
+                return apiResponse;
+            }
+            
             Object.assign(section, sectionDto);
             const updatedSection = await this.sectionRepository.save(section);
             return new ApiResponse<Section>(true, 200, "Section updated successfully", updatedSection);
@@ -39,18 +66,25 @@ export class SectionService {
         }
     }
 
-    async deleteSection(id: string) {
-        const deleteResult = await this.sectionRepository.delete(id);
-        if (deleteResult.affected === 0) {
-            const apiResponse = new ApiResponse<Section>(false, 404, "Section not found");
+    async deleteSection(id: string, user: any) {
+        if(!await this.validateSectionOwnership(id, user.sub)) {
+            const apiResponse = new ApiResponse<Section>(false, 403, "You do not have permission to delete this section");
             return apiResponse;
         }
+
+        const deleteResult = await this.sectionRepository.delete(id);
         const apiResponse = new ApiResponse<Section>(true, 200, "Section deleted successfully");
         return apiResponse;
     }
 
-    async getSectionByFormId(formId: string) {
+    async getSectionByFormId(formId: string, user: any) {
         const sections = await this.sectionRepository.find({ where: { formId } });
+
+        if(!await this.validateFormPublicationStatus(formId) && !await this.validateSectionOwnership(sections[0].id, user.sub)) {
+            const apiResponse = new ApiResponse<Section[]>(false, 403, "You do not have permission to view sections for this form");
+            return apiResponse;
+        }
+
         if (!sections || sections.length === 0) {
             const apiResponse = new ApiResponse<Section[]>(false, 404, "No sections found for this form");
             return apiResponse;
